4b0fe646f28d1db4d599fb176560be8f49eb183f
hongdanh158 <lthdanh@gmail.com>
2017-12-06 14:31:14 +0700

    Update code video

---
+++ video.php
@@ -0,0 +1,308 @@
+<?php include('header.php') ?>
+	<div class="section" id="secondPage">
+		<div class="video-page">
+			<div class="video-wrapper">
+				<div class="video-wrapper-inner">
+					<div class="caption">
+						<h3 class="name">X-MEN FOR BOSS MOTION</h3>
+						<h3 class="name name-2">DÁM HÀNH ĐỘNG - ĐỂ THÀNH CÔNG</h3>
+						<div class="text">Đứng trước những thời khắc quan trọng, đâu là quyết định sáng suốt? Từ bỏ hay Tiếp tục, cũng chỉ là một trong hai lựa chọn sẵn có. Nhưng liệu bạn có dám quyết định táo bạo Từ bỏ để Tiếp tục đón lấy thành công? X-Men for BOSS giới thiệu mùi hương mới X-Men for BOSS Motion - nguồn cảm hứng cho những quyết định táo bạo, dẫn bạn đến thành công.</div>
+						<div class="g-button">
+							 <a href="" class="button  hvr-pulse-grow">Sản phẩm</a>
+							 <a href="" class="button  hvr-pulse-grow">Xem thêm Facebook</a>
+						</div>
+					</div>
+					<div class="video">
+						<!-- video-->
+	                    <video autoplay="true" id="video-default" poster="images/video/poster_video1.jpg" class="video-js vjs-default-skin" controls="controls">
+	                        <source src="https://r1---sn-8qj-8j5y.googlevideo.com/videoplayback?initcwndbps=905000&ip=14.168.45.55&mime=video%2Fmp4&requiressl=yes&mn=sn-8qj-8j5y&pl=23&source=youtube&mm=31&dur=75.766&mv=m&mt=1512467862&ms=au&id=o-AEntG3kgSoypgi9C0gel1D60Fue5JIFabM4nOI02Tinq&expire=1512489562&key=yt6&lmt=1512458774881955&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&ipbits=0&ratebypass=yes&itag=22&ei=-m0mWrvfA4Sm1AaquqzgDQ&signature=819D0E8DEEA8E0491E84D53614E7685AA28F6105.45030C968CF4F34751723C6AFBDDA582036DB3D5" type="video/mp4"/>
+	                    </video>
+	                    <!-- #end video-->
+	                    <h3 class="name">DÁM HÀNH ĐỘNG - ĐỂ THÀNH CÔNG</h3>
+					</div>
+					
+					<div class="clear"></div>
+					<ul class="video-list c-carousel-v has-scroll">
+						<li data-link="https://r7---sn-8pxuuxa-nboel.googlevideo.com/videoplayback?lmt=1496674978637273&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&ratebypass=yes&ipbits=0&key=yt6&signature=CABBF2295B7259ED78D617973FCEF9F4CC8CAFC2.A881F58A630298A25FA2FDBAC74C7B1C5FB5C862&itag=22&ei=cG4mWsX0A8OMoAO48YTICw&pl=21&mn=sn-8pxuuxa-nboel&mm=31&source=youtube&dur=145.705&initcwndbps=680000&mime=video%2Fmp4&ip=115.78.208.28&requiressl=yes&expire=1512489680&mv=m&mt=1512467922&ms=au&id=o-AF7Eg_nDHab6Z7i_7i0uitdmjSaJrhBddZnq356c-QhV" data-poster="images/video/the-movie-buoc-thanh-bai.jpg" class="nv6">
+	                        <div class="wp-img"><img src="images/video/the-movie-buoc-thanh-bai-thumbnail.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>[ X-Men For Boss ][ The Movie ] Bước Thành - Bại</h4>
+	                            <h4 class="f-m">[ X-Men For Boss ][ The Movie ] Bước Thành - Bại</h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://r2---sn-8pxuuxa-nboe7.googlevideo.com/videoplayback?source=youtube&mn=sn-8pxuuxa-nboe7&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpcm2cms%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cexpire&requiressl=yes&ipbits=0&mm=31&ip=115.78.208.28&itag=22&ms=au&mt=1512467922&mv=m&id=o-AEyTAs577e3uuiFcdvec0PU6rohvbaIZ1TydTgKMxvxa&pl=21&ei=nG4mWq68BsX6ogOC2574CA&expire=1512489724&mime=video%2Fmp4&key=yt6&pcm2cms=yes&initcwndbps=647500&signature=39EC1DE87EAAF83A35EAABE40C30B832266A1040.D347D1A299974F6781043B5167195ED9DF83B884&ratebypass=yes&dur=6.060&lmt=1483439286432187" data-poster="images/video/xmen-for-boss-spray.jpg" class="nv6">
+	                        <div class="wp-img"><img src="images/video/xmen-for-boss-spray-thumbnail.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>X-Men For Boss – Body Spray chỉ với 35,000 VNĐ</h4>
+	                            <h4 class="f-m">X-Men For Boss – Body Spray chỉ với 35,000 VNĐ</h4>
+	                        </div>
+	                    </li>
+
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/XMen_For_Boss_Buoc_Thanh_Bai.mp4" data-poster="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/poster_video1.jpg" class="nv1 active">
+	                        <div class="wp-img"><img src="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/thumb_video1.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>X-Men For Boss TVC - Bước Thành Bại</h4>
+	                            <h4 class="f-m">X-Men For Boss TVC - Bước Thành Bại </h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/4a89abcc-a989-4903-b812-1db0dced4830.mp4" data-poster="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/poster_video2.jpg" class="nv2">
+	                        <div class="wp-img"><img src="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/thumb_video2.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>Cách lập kế hoạch kinh doanh</h4>
+	                            <h4 class="f-m"> Cách lập kế hoạch kinh doanh</h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/2f5c7a97-1dba-4635-a241-2b0e4345d71d.mp4" data-poster="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/poster_video3.jpg" class="nv3">
+	                        <div class="wp-img"><img src="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/thumb_video3.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>Chào thành công 2015</h4>
+	                            <h4 class="f-m">Chào thành công 2015</h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/4acc7ca0-cf7f-4c2d-8bfb-a6b1ab400432.mp4" data-poster="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/poster_video4.jpg" class="nv4">
+	                        <div class="wp-img"><img src="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/thumb_video4.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>Hành trình chinh phục <span>'Ý tưởng trăm triệu'</span></h4>
+	                            <h4 class="f-m">Hành trình chinh phục <span>'Ý tưởng trăm triệu'</span></h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/0ba7640b-c965-474c-a736-5ccf9ea96272.mp4" data-poster="images/video/phong-cach-doanh-nhan.jpg" class="nv5">
+	                        <div class="wp-img"><img src="images/video/phong-cach-doanh-nhan-thumbnail.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>Phong cách doanh nhân</h4>
+	                            <h4 class="f-m"> Phong cách doanh nhân</h4>
+	                        </div>
+	                    </li>
+	                    <li data-link="https://storage.googleapis.com/xmenmedia/video/77614682-2218-4609-8234-60acf33b8124.mp4" data-poster="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/poster_video6.jpg" class="nv6">
+	                        <div class="wp-img"><img src="http://x-men.com.vn/static/default/frontend/tmp/xmen_boss/images/boss/thumb_video6.jpg" alt="x-men" class="img-responsive"/></div>
+	                        <div class="caption-vd">
+	                            <h4>X-Men For Boss</h4>
+	                            <h4 class="f-m">X-Men For Boss</h4>
+	                        </div>
+	                    </li>
+	                </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php include('footer.php') ?>
+<script>
+		$(document).ready(function() {
+			$('.homepage .slider-1 .product .item').hover(function() {
+				/* Stuff to do when the mouse enters the element */
+				$('.homepage .slider-1 .product .item').each(function(index, el) {
+					$(this).removeClass('active');
+				});
+				$(this).addClass('active');
+				$('.homepage .slider-1 .sign').each(function(index, el) {
+					if ($(this).hasClass('active')) {
+						$(this).removeClass('active');
+					}
+				});
+				var clss = $(this).attr('sign-class');
+				$(clss).addClass('active');
+			}, function() {
+				/* Stuff to do when the mouse leaves the element */
+				// $('.homepage  .slider-1 .product .item').each(function(index, el) {
+				// 	if($(this).hasClass('active')) {
+				// 		$(this).removeClass('active')
+				// 		var clss = $(this).attr('sign-class');
+				// 		$(clss).addClass('active')
+				// 	}
+				// });
+			});
+		});
+</script>
+<script>
+		$(document).ready(function(){
+		    $(window).on('load resize',function(){
+		        resizeVideo();
+		    });
+		    function resizeVideo() {
+		    	var wVideo = $('.video').width();
+		    	var hVideo = wVideo/2;
+		    	$('.video-js').width(wVideo);
+		    	$('.video-js').height(hVideo);
+		    }
+		});
+		var video = videojs("video-default", {
+			"poster": "images/video/poster_video1.jpg",
+			"autoplay" : true
+		});
+
+		video.on('ended', function () {
+		    this.posterImage.show();
+		    this.bigPlayButton.show();
+		    clearTrackTag();
+		});
+
+		video.on('pause', function () {
+		    this.bigPlayButton.show();
+		});
+
+		video.on('play', function () {
+		    this.trigger('loadstart');
+		    this.posterImage.hide();
+		    this.bigPlayButton.hide();
+		});
+		var trackerObj = {};
+
+		var ebVideoStarted = "http://log.adtimaserver.vn/track/hit.gif?params=x20mlbps7s_F0hL5OGffeLi3rsq5MqzhxHEuRj1HB-RtZyaGVxuln-lIoeRvXqGWsKwE20OTQhsFDa7KPz4TJ7ClRUNHRkUE_nAupf1HIGHDT4A9nv-lc8oXbFPFM1OAwW4DgMRBYh2_xS1TUbCOA4AP4dZX71WPW-RxYYpSqZI72h0ZXRFqaS4&uid=";
+		var eb25Per_Played = "http://log.adtimaserver.vn/track/hit.gif?params=8VsQlbps7s_zvNL5OGffTkLfPgo5MprzLOEOxj1x_QwN5yaGYQFNj8nIodKdEomG0OwyqS9TwhsFO4ASan-6EXPERmR5YokqEy_Uhe4MiLrn7mIVvkn_n6IGKfoCjN7ueL3rRf-VBc-zgZl2ooajAGHrgocv1Y811nW52hIp_B0G_RldEQSfNg&uid=";
+		var eb50Per_Played = "http://log.adtimaserver.vn/track/hit.gif?params=TGYf814k_u9O25ThiKdfc795Xgo5MqfmiCkehj1MGkjFh6aGXB5pR2lIoe94xoEWsKwPzMOTQhsFBte9Q0JDbzGhG5cAwWN8P9jYO-HEIgsUM6ng_fs028-Chy-zpg4M2uzciI3FTrGqnN3AV4DyJiNlDI-Chy_V6lJKdbnaEkzbK5qdGV10q5H-&uid=";
+		var eb75Per_Played = "http://log.adtimaserver.vn/track/hit.gif?params=wWsgtTps7s_D7BL5OGfffrL_vgo5Mqrj7GEOxj1BDaxN5yaGURENj8nIoeJtcgmG0OwC2W9TwhsFC9DyqfJVObXH3_UPM4emaXhdM9cWbHjJA7gN_nmvv-IHL9DoctydbwLEApi96Bq8OIF6XgrWYwAJ6woXb1WPXeZdmVoSKZQ9Ps1Z3RFsb04&uid=";
+		var ebVideoFullPlay = "http://log.adtimaserver.vn/track/hit.gif?params=oIpD-76k_u-ij6xRKCdfn5NpHoo5MktCWOmehj2grhAFh6aGsLKBV4nIoQNM7mGWsKzj-PPzAhsF6vNNxEdmUyZkY8JDb1UHSO8yhewFaoTThMxu1TeIm8-ChyxfFBYM2ubl-sVWg-xVrQ9I7jCeJiOJ6D_iBy_VBk7iFbnaEqB3y5qdGV2Yj2ku&uid=";
+
+		var timeTrackingArr = [
+		    {time: 3, link: 'http://log.adtimaserver.vn/track/hit.gif?params=ZkwBQ76k_u9kzYQhiKdfWVmhDkq5Mo2AMHm-Bj1m4FCFh6aGdvQxl4nIocWOyikG0OwlutdDopsFLLFldQfHRgoqwAt2nBkttvYxrIbsMjgG9yvg_dFCzz_iBy-ftap9uN82XlP0PZxFaVnf4Ytg7oPPNpvql29VwAQ6tTnaEmYx88rdmV1esbnu&uid='},
+		    {time: 10, link: 'http://log.adtimaserver.vn/track/hit.gif?params=flQJ814k_u98pXRhiKdfQXFBDkq5MpWIQDk-Bj1-yHDFh6aGbgyBV4nIod2WiikG0Ow9kqfj4psFNImk8C2sjYuQPMBvKd6g93s9FgLqIgI2t5jN_kbi2-qXbFb4IbJE8WOJBVL1NI3dwBEWcfiew4A2hjbX71WPYtujnZIC6SsSPs1Z3RFTcAo&uid='},
+		    {time: 30, link: 'http://log.adtimaserver.vn/track/hit.gif?params=81kSjfps7s_xvvrpGOffTETn7kq5Mpj1NLGuRj1z8xwtZyaGYwctD8nIodCbNsEWsKwwr1NzAhsFOYL7OBEpmc5cHR9AR28IXMJlfUdFHHlcFCCWhcR_W8-Chy-NXjk52hEv3HhVPsdsXcmTK9KypiNaOy_Chy_V1X-2HRlaEnMibX5NWd1LQhce&uid='},
+		    {time: 30, link: 'http://log.adtimaserver.vn/track/hit.gif?params=Zc-EPZrs7s9nMIqpGOff2tav_oo5Mg4PLOEOxj3lYUQtZyaG9XVFH-lIoUYByikG0OymJc_TwhsFrwxja1CH75vCnLjohZiScNlGdiX8gK3_RkVcndLN1__iBy8bz4fHT7SHJJfeFi75uBRxIp-IvkNMteOql29VQ4kWnRlaEuWwhe4N2d3dME_O&uid='} // pls don't change it :)
+		];
+
+		video.on('timeupdate', function () {
+		    var del = Math.round((video.currentTime() / video.duration()) * 100);
+		    var currentTime = video.currentTime();
+
+		    for (var i = 0; i < timeTrackingArr.length; i++)
+		    {
+		        var trackVO = timeTrackingArr[i];
+		        if (currentTime >= trackVO.time && trackVO.link && !trackVO.isTrack)
+		        {
+		            console.log("TRACKING: " + trackVO.time + '_Played');
+		            loadTracker(trackVO.link);
+		            trackVO.isTrack = true;
+		        }
+		    }
+
+		    if (del == 100 && !trackerObj["ebVideoFullPlay"] && ebVideoFullPlay) // 100%
+		    {
+		        console.log("TRACKING: ebVideoFullPlay");
+		        trackerObj["ebVideoFullPlay"] = true;
+		        loadTracker(ebVideoFullPlay);
+		    } else if (del >= 75 && !trackerObj["eb75Per_Played"] && eb75Per_Played) // 75%
+		    {
+		        console.log("TRACKING: eb75Per_Played");
+		        trackerObj["eb75Per_Played"] = true;
+		        loadTracker(eb75Per_Played);
+		    } else if (del >= 50 && !trackerObj["eb50Per_Played"] && eb50Per_Played) // 50%
+		    {
+		        console.log("TRACKING: eb50Per_Played");
+		        trackerObj["eb50Per_Played"] = true;
+		        loadTracker(eb50Per_Played);
+		    } else if (del >= 25 && !trackerObj["eb25Per_Played"] && eb25Per_Played) // 25%
+		    {
+		        console.log("TRACKING: eb25Per_Played");
+		        trackerObj["eb25Per_Played"] = true;
+		        loadTracker(eb25Per_Played);
+		    } else if (del >= 0 && !trackerObj["ebVideoStarted"] && ebVideoStarted) // 0%
+		    {
+		        console.log("TRACKING: ebVideoStarted");
+		        trackerObj["ebVideoStarted"] = true;
+		        loadTracker(ebVideoStarted);
+		    }
+		});
+
+		function loadTracker(param) {
+		    if (Array.isArray(param))
+		    {
+		        for (var i = 0; i < param.length; i++)
+		        {
+		            iTracker(param[i]);
+		        }
+		    } else
+		    {
+		        iTracker(param);
+		    }
+
+		}
+
+		function iTracker(val)
+		{
+		    if (val.length > 0) {
+		        var rnd = Math.floor(Math.random() * 10000000000);
+		        val = val.replace('[timestamp]', rnd);
+
+		        var img_tracking = document.createElement('img');
+		        img_tracking.src = '' + val;
+		        img_tracking.style.height = '0px';
+		        img_tracking.style.width = '0px';
+		        img_tracking.style.display = 'none';
+		        document.body.appendChild(img_tracking);
+		        console.log("iTracker: " + val);
+		    }
+		}
+
+		function clearTrackTag() {
+		    trackerObj["ebVideoStarted"] = false;
+		    trackerObj["eb25Per_Played"] = false;
+		    trackerObj["eb50Per_Played"] = false;
+		    trackerObj["eb75Per_Played"] = false;
+		    trackerObj["ebVideoFullPlay"] = false;
+
+		    for (var i = 0; i < timeTrackingArr.length; i++) {
+		        var trackVO = timeTrackingArr[i];
+		        trackVO.isTrack = false;
+		    }
+		}
+		video.on('ended', function () {
+		this.posterImage.show();
+		this.bigPlayButton.show();
+		});
+		video.on('play', function () {
+		this.bigPlayButton.hide();
+		this.posterImage.hide();
+		});
+		video.on('pause', function () {
+		this.trigger('loadstart');
+		this.posterImage.hide();
+		this.bigPlayButton.show();
+		});
+	</script>
+	<script>
+		if(window.innerWidth > 1025){
+			$(document).ready(function() {
+				// var video_element = document.getElementById('video-default');
+				$('#fullpage-xit').fullpage({
+					verticalCentered: true,
+					anchors: ['video','product'],
+					menu: '.c-icon-scrolldown',
+					afterLoad: function(anchorLink, index) {
+						if (index == 1) {
+							$(".s-section-slide").removeClass('active');
+							$(".c-icon-scrolldown2").fadeIn();
+							video.pause();
+							setTimeout(function() {
+								$('.s-video-wrapper-v').addClass('show');
+							}, 1000);video.play();
+
+						}
+						if (index == 2) {
+							setInterval(function(){
+								var vid = document.getElementById('vid1');vid.play();
+							}, 1000);
+							$(".s-section-slide").removeClass('active');
+							$(".c-icon-scrolldown2").fadeOut();
+						}
+					}
+				});
+
+			});
+
+		}
+		$('.video-list li').click(function () {
+			var id = $(this).attr('data-link');
+			var poster = $(this).attr('data-poster');
+			var name = $(this).find('h4').eq(0).html();
+			var srcPoster = poster;
+			$(".vjs-poster").attr('style', 'background-image:url(' + srcPoster + ')');
+			var src = id;
+			$('.video-wrapper-inner .video .name').html(name);
+			$("#video-default video").attr('poster', srcPoster);
+			$("#video-default video").attr('src', src), setTimeout(function() {
+				video.play();
+			}, 100);
+		});
+	</script>
+</body>
+</html>
